dd610c352824230f9e67d3e09f57706f3b27bfbf
s1u2k3 <bsukhendu48@gmail.com>
2025-08-12 20:42:13 +0000

    variable declaration

---
+++ basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 7709
+let accountEmail = "sukhen@gmail.com"
+var accountPass = "9889"
+accountCity = "jaypur"
+
+// accountId = 5678 // not allowed
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPass,accountCity])
+
+/* prefer not to use var due to block scope and function scope
+*/
