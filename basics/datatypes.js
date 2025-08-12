e88a14fdc6f88b686f2ac2fbaa68a25baeceda4f
s1u2k3 <bsukhendu48@gmail.com>
2025-08-12 21:10:38 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict"       // treat all code as newer version
+
+alert(3 +3)    // we are using node js not browser
+
+// ecmascript documentation and mdn documentation for javascript document
+
+let name = "sukhen"
+
+let age = 18
+
+let value = true
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/faklse'
+null => standalone value
+undefined
+symble => unique
+
+object 
+
+*/
+
+
+console.log(typeof(null))       // object
+console.log(typeof(undefined));  // undefined
+
+
