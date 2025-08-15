2e7b48c16f600161c3f8b021e378685c6cd8a219
s1u2k3 <bsukhendu48@gmail.com>
2025-08-15 19:08:50 +0000

    comparison_data

---
+++ basics/comparison.js
@@ -0,0 +1,20 @@
+console.log( null > 0)
+console.log( null < 0)
+console.log( null >= 0)
+console.log( null <= 0)
+
+console.log( undefined > 0)
+console.log( undefined < 0)
+console.log( undefined >= 0)
+console.log( undefined <= 0)
+console.log( undefined > 0)
+
+console.log( "2" > 1)
+console.log( "1" > 0)
+
+
+// === i does't conversion operation and check also datatypes
+
+console.log( "2" === 1) 
+
+
