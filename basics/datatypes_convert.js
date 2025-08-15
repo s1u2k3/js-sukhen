d37eea2154fff866284e355e9972dd0dad6d3d85
s1u2k3 <bsukhendu48@gmail.com>
2025-08-15 18:31:03 +0000

    conversion_datatypes

---
+++ basics/datatypes_convert.js
@@ -0,0 +1,47 @@
+let score = "33";
+
+console.log(typeof(score));   // that time it is a string but now it will convert to number.
+
+let convertion = Number(score)
+
+console.log(typeof(convertion));
+
+let str = "33abc";
+
+let contion = Number(str)
+
+console.log(typeof(contion));
+
+console.log(contion);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1 || False => 0
+// null =>0 
+// undefined => NaN
+
+
+let value = 1
+
+let boolean = Boolean(value)
+
+console.log(boolean)
+
+
+//  1 => true || 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let num = 45
+
+let st = String(num)
+
+console.log(st)
+
+console.log(typeof(st))
+
+
+
