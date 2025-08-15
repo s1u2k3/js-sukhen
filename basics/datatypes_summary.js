224f96d9cbac58f10ec175f740ea72a1ed8bdb69
s1u2k3 <bsukhendu48@gmail.com>
2025-08-15 20:07:20 +0000

    summary_datatypes

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,10 @@
+// Primitive datatypes
+
+// 7 types => String, Number, Boolean, null, undefined, Symbol
+
+// Referenc (non-primitive)
+
+// Array, Object, Functions
+
+
+// javascript is a dynamic type language because here don't require to define values types
